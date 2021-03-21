9d01f53e510e147c07a9cefff43a2201634f4184
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2021-03-21 16:04:16 -0700

    initial commit

---
+++ public/app.js
@@ -0,0 +1,3 @@
+
+$('body').css("background-color","blue");
+let appDiv = document.getElementById('app');
---
+++ server/express.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.static(__dirname + '/../public'));
+
+
+app.get('/', async (req, res) => {
+  let appDiv = document.getElementById('app');
+
+});
+
+
+
+app.listen(PORT, () => {
+  console.log('express listening on port ' + PORT)
+});
